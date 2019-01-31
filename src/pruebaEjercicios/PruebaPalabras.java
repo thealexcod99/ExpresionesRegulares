db6d058ded1e8ecc777e1cff620380a9c2390615
Alvaro Cano Galino <alvaro-galiano-99@hotmail.com>
2019-01-31 08:47:07 +0100

    Hola

---
+++ src/pruebaEjercicios/PruebaPalabras.java
@@ -0,0 +1,26 @@
+package pruebaEjercicios;
+
+import java.util.Scanner;
+
+import ejercicios.Palabra;
+
+public class PruebaPalabras {
+
+	public static void main(String[] args) {
+		System.out.println("introduce una palabra");
+		Scanner sc = new Scanner(System.in);
+		String palabra = sc.next();
+		sc.close();
+		if (palabra.toLowerCase().matches("[a-zñáéóíúü]+")) {
+			Palabra p = new Palabra(palabra);
+			System.out.printf("La palabra %s tiene %d letras, tiene %d vocales, "
+					+ "empieza por vocal %b, acabe en vocal %b y es un palíndromo %b%n", 
+					palabra, p.obtenerNumeroLetras(), p.obtenerNumeroVocales(), 
+					p.empiezaPorVocal(), p.acabaEnVocal(), p.esPalindromo());
+		}
+		else
+			System.out.printf("%s no es una palabra%n", palabra);
+
+	}
+
+}
--- src/pruebasTeoria/PruebaColeccionPersonas.java
+++ src/pruebasTeoria/PruebaColeccionPersonas.java
@@ -1,62 +1,60 @@
 package pruebasTeoria;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import teoria.Persona;
 
+//gabriel jesús ; gonzález  del amo; martínez; 25; 25.25
+//jesús ; gonzález ; martínez; 55; 55,25
+//jesús ; gonzález ; martínez; 5dfsdfsd; 55,25    Este no se usar para crear objeto Persona
+
+
 public class PruebaColeccionPersonas {
 
 	public static void main(String[] args) {
-
-		//Persona p = new Persona(null, null, null, 0, 0);
-
+		// TODO Auto-generated method stub
+		List<Persona> listaPersonas = new ArrayList<>();
+		System.out.println("Introduce nombre, apellidos, edad y peso");
 		Scanner sc = new Scanner(System.in);
-
+		sc.useDelimiter("[;\\n]");
+		String nombresRegex = "[a-zA-ZñÑáÁéÉíÍóÓúÚüÜ\\s]+";
+		String enteroRegex  = "\\d+";  // [0-9]+
+		String realRegex    = "([\\d]+|[\\d]+\\.[\\d]+)";   // 2 34  2,26  22,5888
 		String nombre, apellido1, apellido2, edad, peso;
-
+		int edadNumero;
+		float pesoNumero;
+		//Persona persona;
 		for (int i = 0; i < 5; i++) {
+			nombre = sc.next().trim();
+			apellido1 = sc.next().trim();
+			apellido2 = sc.next().trim();
+			edad   = sc.next().trim();
+			peso   = sc.next().trim();
 
-
-			System.out.println("Introduce el nombre:");
-			nombre = sc.next();
-			if (!nombre.matches("\\s")) {
-				System.out.println("Nombre no válido");
-				System.out.println("Introduce el nombre:");
-				nombre = sc.nextLine();
+			if ( edad.matches(enteroRegex) && peso.matches(realRegex) ) {
+				edadNumero = Integer.parseInt(edad);  //entran nada mas que nº enteros
+				pesoNumero = Float.parseFloat(peso);  //entran nada mas que nº reales
+				if (nombre.matches(nombresRegex) && apellido1.matches(nombresRegex) &&
+						apellido2.matches(nombresRegex) && (edadNumero > 0 && edadNumero < 150)
+						&& (pesoNumero > 0 && pesoNumero < 400)) {
+//					persona = new Persona(nombre, apellido1, apellido2, edadNumero, pesoNumero);
+//					listaPersonas.add(persona);
+					listaPersonas.add(new Persona(nombre, apellido1, apellido2, 
+							edadNumero, pesoNumero));
+					
+				}
 			}
-
-			System.out.println("Introduce el primer apellido:");
-			apellido1 = sc.next();
-			if (!apellido1.matches("\\s")) {
-				System.out.println("Apellido no válido");
-				System.out.println("Introduce el primer apellido:");
-				apellido1 = sc.nextLine();
-			}
-
-			System.out.println("Introduce el segundo apellido:");
-			apellido2 = sc.next();
-			if (!apellido2.matches("\\s")) {
-				System.out.println("Apellido no válido");
-				System.out.println("Introduce el segundo apellido:");
-				apellido2 = sc.nextLine();
-			}
-
-			System.out.println("Introduce el peso:");
-			peso = sc.next();
-			if (!peso.matches("")) {
-				System.out.println("Peso no válido");
-				System.out.println("Introduce el peso:");
-				peso = sc.nextLine();
-			}
-
-			System.out.println("Introduce la edad:");
-			edad = sc.next();
-			if (!edad.matches("^0?[1-9]|^[1-9][\\d]")) {
-				System.out.println("Edad no válida");
-				System.out.println("Introduce la edad:");
-				edad = sc.nextLine();
-			}
+			//			System.out.printf("Leido: %S %S, %s %s %s%n", 
+			//					apellido1, apellido2, nombre, edad, peso);
 		}
+		sc.close();
+		//System.out.println(listaPersonas);
+//		for (Persona persona : listaPersonas) {
+//			System.out.println(persona);
+//		}
+		listaPersonas.forEach(System.out::println);
 	}
 
 }
